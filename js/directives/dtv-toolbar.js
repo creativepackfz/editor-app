192f659097f909942360afc5c3e0b776fefedde2
Alex Deaconu <alex.deaconu@gmail.com>
2015-08-13 17:45:01 -0400

    Converted presentation html parser utils to JS

--- js/directives/dtv-toolbar.js
+++ js/directives/dtv-toolbar.js
@@ -1,29 +1,29 @@
 'use strict';
 
 angular.module('risevision.editorApp.directives')
-  .directive('toolbar', [ 'placeholdersFactory', '$modal',
+  .directive('toolbar', ['placeholdersFactory', '$modal',
     function (placeholdersFactory, $modal) {
       return {
         restrict: 'E',
         templateUrl: "partials/toolbar.html",
         link: function ($scope) {
 
-          $scope.addNewPlaceHolder = function () {
-            placeholdersFactory.addNewPlaceHolder();
-          }
+            $scope.addNewPlaceHolder = function () {
+              placeholdersFactory.addNewPlaceHolder();
+            }
 
-          $scope.preview = function () {
-            //TODO preview
-            console.log('preview');
-          }
+            $scope.preview = function () {
+              //TODO preview
+              console.log('preview');
+            }
 
-          $scope.openProperties = function () {
-            $modal.open({
-              templateUrl: 'partials/presentation-properties-modal.html',
-              controller: 'PresentationPropertiesModalController'
-            });
-          }
-        } //link()
+            $scope.openProperties = function () {
+              $modal.open({
+                templateUrl: 'partials/presentation-properties-modal.html',
+                controller: 'PresentationPropertiesModalController'
+              });
+            }
+          } //link()
       };
     }
   ]);
