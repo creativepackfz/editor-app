e0911c44623ebdcff046da28f901ff856130d6cb
ezequiel <ezequielc@gmail.com>
2015-08-25 11:28:52 -0300

    Placeholder playlist items list and remove

--- js/directives/dtv-placeholder-playlist.js
+++ js/directives/dtv-placeholder-playlist.js
@@ -1,14 +1,40 @@
 'use strict';
 
 angular.module('risevision.editorApp.directives')
-  .directive('placeholderSettings', ['placeholderFactory',
-    function (placeholderFactory) {
+  .directive('placeholderPlaylist', ['placeholderPlaylistFactory', '$modal', '$templateCache',
+    function (placeholderPlaylistFactory, $modal, $templateCache) {
       return {
         restrict: 'E',
         scope: true,
-        templateUrl: 'partials/placeholder-settings.html',
+        templateUrl: 'partials/placeholder-playlist.html',
         link: function ($scope) {
-          $scope.factory = placeholderFactory;
+          $scope.factory = placeholderPlaylistFactory;
+
+          $scope.remove = function (item) {
+              var modalInstance = $modal.open({
+                template: $templateCache.get(
+                  'confirm-instance/confirm-modal.html'),
+                controller: 'confirmInstance',
+                windowClass: 'modal-custom',
+                resolve: {
+                  confirmationTitle: function () {
+                    return 'Remove Item';
+                  },
+                  confirmationMessage: function () {
+                    return 'Are you sure you want to remove ' +
+                      'this Content from the Playlist?';
+                  },
+                  confirmationButton: function () {
+                    return 'Remove';
+                  },
+                  cancelButton: null
+                }
+              });
+
+              modalInstance.result.then(function () {
+                placeholderPlaylistFactory.removeItem(item);
+              });
+            };
 
         }
       };
